2964bdbfa3df56e225e420d8bb156c79eaae3075
Asyafa Ditra Al Hauna <asyafaditra@gmail.com>
2023-04-02 21:34:01 +0700

    first

---
+++ hello World/hello.java
@@ -0,0 +1,23 @@
+class hello // hello : nama kelas harus sama dengan nama file nya
+{
+    public static void main(String[] arguments) 
+    {
+        System.out.println("hello World");
+    }
+}
+/*
+ * static : static method named main
+ * public : has public access
+ * String[] : takes array of string as a parameter
+ * doesnt return any value
+ * arguments : the name of the args array (default args)
+ */
+
+
+
+// class orodo // hello : nama kelas harus sama dengan nama file nya
+// {
+//     public static void main(String[] args) {
+//         System.out.println("hello World");
+//     }
+// }
---
+++ variables/variables.java
@@ -0,0 +1,57 @@
+package variables;
+
+public class variables {
+    public static void main(String[] args)
+    {
+        // int speedLimit = 50;
+        // System.out.println(speedLimit);
+        
+        // speedLimit = 0;
+        // System.out.println(speedLimit);
+        
+        //? java literal
+        // literals are data used to representing fixed value
+        
+        int a = 1;
+        float b = 2.5f;
+        char c = 'F';
+        boolean flag1 = true;
+        boolean flag2 = false;
+        
+        int binaryNumber = 0b10010;
+        // System.out.println(binaryNumber);
+        
+        int octalNumber = 027;
+        // System.out.println(octalNumber);
+        
+        int decimal = 34;
+        
+        int hexadecimal = 0x2F;
+        // System.out.println(hexadecimal);
+        
+        double myDouble = 3.4;
+        float myFloat = 3.4F;
+        
+        // 3.445 * 10 ^ 2
+        double myDoubleScientific = 3.445e2;
+
+        // System.out.println(myDouble);
+        // System.out.println(myFloat);
+        // System.out.println(myDoubleScientific);
+        
+        char letter = 'a';
+        letter = '\b'; // backspace
+        letter = '\t'; // tab
+        letter = '\n'; // newline
+
+        String str1 = "Java Programming";
+        
+        // System.out.println(letter);
+
+
+
+
+
+
+    }
+}
