3c5c491da2b42929b1e5dd1ec0b2ce8ada9e13cc
Asyafa Ditra Al Hauna <asyafaditra@gmail.com>
2023-04-04 16:52:05 +0700

    flow control finish

--- flowControl/breakk.java
+++ flowControl/breakk.java
@@ -1,8 +1,54 @@
 package flowControl;
+import java.util.Scanner;
 
 public class breakk {
     public static void main(String[] args) {
+        // int i = 0;
+
+        // for(i = 0; i < 10; i++)
+        // {
+        //     System.out.println(i);
+        //     if (i == 5)
+        //     {
+        //         System.out.println("STOP");
+        //         break;
+        //     }
+        // }
+
+        // Scanner input = new Scanner(System.in);
+        // double num = 0, summ = 0;
         
+        // while(true)
+        // {
+        //     summ += num;
+        //     System.out.print("Enter a number : ");
+        //     num = input.nextDouble();
+
+        //     if (num < 0.0) {
+        //         System.out.print(summ);
+        //         break;
+        //     }
+        // }
+        
+        // input.close();
+
+        // int i = 0, j = 0, k = 0;
+
+        // first:
+        // while(i < 3)
+        // {
+        //     while(j < 3)
+        //     {
+        //         while (k < 3) {
+        //             System.out.println(i + " " + j + " " + k);
+        //             k++;
+        //         }
+        //         System.out.println(i + " " + j);
+        //         j++;
+        //         break first;
+        //     }
+        //     System.out.println(i);
+        //     i++;
+        // }
     }
 }
-// C:\Program Files\Java\jdk1.8.0_321\bin
---
+++ flowControl/continu.java
@@ -0,0 +1,101 @@
+package flowControl;
+
+import java.util.Scanner;
+
+public class continu {
+    public static void main(String[] args) {
+        // int i = 0;
+
+        // for(i = 0; i <= 10; i++)
+        // {
+        // if(i > 4 && i < 9)
+        // {
+        // continue;
+        // }
+        // System.out.println(i);
+        // }
+
+        // Scanner input = new Scanner(System.in);
+
+        // double summ = 0, num = 0;
+
+        // for(int i = 0; i < 5; i++)
+        // {
+        // System.out.print("The number : ");
+        // num = input.nextDouble();
+
+        // if (num < 0.0) {
+        // continue;
+        // }
+
+        // summ += num;
+
+        // }
+
+        // System.out.println("The sum = " + summ);
+
+        // int o = 1, p = 1;
+
+        // while (o < 4)
+        // {
+        // System.out.println("Outer Loop: " + o);
+
+        // while(p < 4)
+        // {
+
+        // if (p != 2) {
+        // System.out.println("inner Loop : " + p);
+        // }
+
+        // else
+        // {
+        // p++;
+        // continue;
+        // }
+        // p++;
+
+        // }
+        // o++;
+        // }
+
+        // ! labeled continue
+        // int r = 0, s = 0;
+
+        // first:
+        // while(r < 5)
+        // {
+        // r++;
+        // if (r == 3)
+        // {
+        // continue first;
+        // }
+
+        // while(s < 5)
+        // {
+        // s++;
+        // if(s ==2)
+        // {
+        // s = 0;
+        // continue first;
+        // }
+        // System.out.println("i = " + r + "; " + "j = " + s);
+        // }
+        // }
+
+        // int t = 0, u = 0;
+
+        // first: while (t < 5) {
+        //     t++;
+
+        //     while (u < 5) {
+        //         u++;
+        //         if (t == 3 || u == 2) {
+        //             u = 0;
+        //             continue first;
+        //         }
+        //         System.out.println("i = " + t + "; " + "j = " + u);
+        //     }
+        // }
+
+    }
+}
