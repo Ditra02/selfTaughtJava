0b8aa81c369b8b2a1c3feeb87490a87f372c3c60
Asyafa Ditra Al Hauna <asyafaditra@gmail.com>
2023-04-04 11:06:48 +0700

    second

---
+++ flowControl/breakk.java
@@ -0,0 +1,8 @@
+package flowControl;
+
+public class breakk {
+    public static void main(String[] args) {
+        
+    }
+}
+// C:\Program Files\Java\jdk1.8.0_321\bin
--- flowControl/loop.java
+++ flowControl/loop.java
@@ -2,50 +2,50 @@ package flowControl;
 
 public class loop {
     public static void main(String[] args) {
-        // int n = 4;
+        int n = 4;
 
-        // for (int i = 0; i <= 4; i++)
-        // {
-        //     System.out.println("idx = " + i);
-        // }
+        for (int i = 0; i <= 4; i++)
+        {
+            System.out.println("idx = " + i);
+        }
 
-        // int sum = 0;
-        // int i;
-        // for (i = 0; i < 1000; i++) {
-        //     if (i % 2 == 0) {
-        //         sum += i;
-        //         System.out.println(i);
-        //     }
-        // }
+        int sum = 0;
+        int i;
+        for (i = 0; i < 1000; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
 
-        // System.out.println("Sum = " + sum);
+        System.out.println("Sum = " + sum);
 
-        // int[] numbers = { 3, 7, 5, -5 };
+        int[] numbers = { 3, 7, 5, -5 };
 
-        // for(int number: numbers)
-        // {
-        //     System.out.println(number);
-        // }
+        for(int number: numbers)
+        {
+            System.out.println(number);
+        }
 
         // ? for each loop
-        // int[] array = {1, 2, 3, 4, 5};
-        // int sumArr = 0;
+        int[] array = {1, 2, 3, 4, 5};
+        int sumArr = 0;
 
-        // for (int angka: array)
-        // {
-        //     sumArr += angka;
-        // }
+        for (int angka: array)
+        {
+            sumArr += angka;
+        }
         
-        // System.out.println("sum array = " + sumArr);
+        System.out.println("sum array = " + sumArr);
 
-        // int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
-        // for(int i = 0; i < array2.length; i++)
-        // {
-        //     System.out.println("index " + i + " >> " + array2[i]);
-        // }
+        for(int i = 0; i < array2.length; i++)
+        {
+            System.out.println("index " + i + " >> " + array2[i]);
+        }
         
-        // System.out.println(array2.length);
+        System.out.println(array2.length);
 
     
     
